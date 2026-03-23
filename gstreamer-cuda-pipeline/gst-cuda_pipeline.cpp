2dec2762a19b31992f8dfc150e66ebdd120701cb
vilchez <dievilchez23@gmail.com>
2026-03-23 16:19:16 -0600

    add initial webcam pipeline

--- gstreamer-cuda-pipeline/gst-cuda_pipeline.cpp
+++ gstreamer-cuda-pipeline/gst-cuda_pipeline.cpp
@@ -1,52 +1,48 @@
-// gst-cudapipeline.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// gst-cudapipeline.cpp : this file contains the 'main' function. program execution begins and ends there.
 
 #include <iostream>
 #include <gst/gst.h>
 
-/*
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-*/
+int main(int argc, char* argv[]) {
 
-int tutorial_main(int argc, char* argv[]) {
-
-    GstElement* pipeline;
+    GstElement* pipeline, * source, * convert, * sink;
     GstBus* bus;
     GstMessage* msg;
 
-    /* Initialize GStreamer */
     gst_init(&argc, &argv);
 
-    /* Build the pipeline */
-    pipeline =
-        gst_parse_launch
-        ("playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
-            NULL);
+    source = gst_element_factory_make("mfvideosrc", "source");
+    sink = gst_element_factory_make("autovideosink", "sink");
+    convert = gst_element_factory_make("videoconvert", "convert");
+    pipeline = gst_pipeline_new("webcam-pipeline");
 
-    /* Start playing */
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-
-    /* Wait until error or EOS */
-    bus = gst_element_get_bus(pipeline);
-    msg =
-        gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
-
-    /* See next tutorial for proper error message handling/parsing */
-    if (GST_MESSAGE_TYPE(msg) == GST_MESSAGE_ERROR) {
-        g_printerr("An error occurred! Re-run with the GST_DEBUG=*:WARN "
-            "environment variable set for more details.\n");
+    if (!pipeline || !source || !convert || !sink) {
+        g_printerr("Not all elements could be created.\n");
+        return -1;
     }
 
-    /* Free resources */
-    gst_message_unref(msg);
+    gst_bin_add_many(GST_BIN(pipeline), source, convert, sink, NULL); //Null, stops reading arguments 
+
+
+    if (!gst_element_link_many(source, convert, sink, NULL)) {
+        g_printerr("Elements could not be linked.\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+
+    bus = gst_element_get_bus(pipeline);
+    msg = gst_bus_timed_pop_filtered(bus,GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+    if (msg != NULL) {
+        gst_message_unref(msg);
+    }
+
     gst_object_unref(bus);
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
-    return 0;
-}
 
-int main(int argc, char* argv[]) {
-    return tutorial_main(argc, argv);
+    return 0;
+
 }
